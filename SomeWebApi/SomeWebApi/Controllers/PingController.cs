8f0cf0476a713ca84df3013d1d3674b6d4801877
nfrank <nils.frank@reichhart.eu>
2017-12-29 12:24:36 +0100

    still init

---
+++ SomeWebApi/SomeWebApi/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace SomeWebApi.Controllers
+{
+    public class PingController : ApiController
+    {
+        [HttpGet]
+        [Route("ping/isalive")]
+        public IHttpActionResult Get_IsAlive()
+        {
+            return Ok("Im Alive!");
+        }
+    }
+}
